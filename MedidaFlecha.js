fabba306704e5b3ad1d73c8f9cf76eb8382874b1
Alkivar Rueda <61318487+Alkivar@users.noreply.github.com>
2021-03-25 13:49:33 -0500

    Update MedidaFlecha.js

--- MedidaFlecha.js
+++ MedidaFlecha.js
@@ -1,8 +1,11 @@
-let CalcularDistancia=(DistanciaFinalX,DistanciaInicialX,DistanciaFinalY,DistanciaInicialY)=>{
-    let SumaTotal=(Math.pow(DistanciaFinalX-DistanciaInicialX,2)) + (Math.pow(DistanciaFinalY-DistanciaInicialY,2));
-    return Math.sqrt(SumaTotal,2);
+let CalcularDistancia=(DistanciaFinalX,DistanciaInicialX,DistanciaFinalY,DistanciaInicialY) =>{
+
+    let SumaTotal = (Math.pow(DistanciaFinalX-DistanciaInicialX,2)) + 
+                    (Math.pow(DistanciaFinalY-DistanciaInicialY,2));
+
+    console.log (Math.sqrt(SumaTotal,2));
 }
-console.log(CalcularDistancia(70,0,50,0));
+    CalcularDistancia(70,0,50,0);
 
 
 
